4bcf083ff15cb77f1841574479c4d282254cb049
chrischen <y_zhengchen@163.com>
2022-06-23 13:32:56 +0800

    missing part提交

--- src/main/java/com/airuolin/project/Application.java
+++ src/main/java/com/airuolin/project/Application.java
@@ -1,8 +1,11 @@
-package com.company.project;
+package com.airuolin.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author chrischen
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
